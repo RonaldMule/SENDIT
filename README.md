c16fd70ffc4b28501b35117089c0422d0d4b86a1
https://github.com/RonaldMule/StoreManager <ronaldmulyowa@gmail.com>
2018-11-13 11:30:20 +0300

    create parcel delivery order [finishes#161876604]

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-*SENDIT
+#SENDIT API endpoints
+SendIT is a courier service that helps users deliver parcels to different destinations. SendIT
+provides courier quotes based on weight categories.
+
+##End points
+
+```
+
+|           EndPoint            |                  Functionality                           |
+
+|     GET /parcels              |           Fetch all parcel delivery orders               |
+|GET /parcels/<parcelId>        |   Fetch a specific parcel delivery order                 |
+|GET /users/<userId>/parcels    |  Fetch all parcel delivery orders by a specific user     |
+
+|PUT /parcels/<parcelId>/cancel |  Cancel the specific parcel delivery order               |
+|POST /parcels                  |  Create a parcel delivery order                          |
+
+```
---
+++ endpoints.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify  
+from models.model import Parcel, parcel_order_list               
+app = Flask(__name__)
+
+parcels = []
+@app.route("/api/v1/parcels", methods =['GET','POST'])
+def parcelOrders():
+   if request.method == 'GET':
+       #call the method to get all parcel orders
+       return getAllParcelOrders()
+   elif request.method == 'POST':
+        #call the method to make new parcel order
+        return makeNewParcelOrder()
+
+
+
+
+
+def getAllParcelOrders():
+
+    return jsonify({'parcels':parcel_order_list}), 200  
+
+def makeNewParcelOrder():
+    data = request.get_json(force=True)
+
+    try:
+       parcels = Parcel(parcel_weight=data['parcel_weight'], pickup_location=data['pickup_location'], 
+       destination=data['destination'], price=data['price'], status=data['status'])
+       parcels.to_json()
+    except ValueError as e:
+        print(e)
+        return jsonify({'message': 'price should be an integer'}), 400
+    return jsonify({'message': 'post successful'}), 201   
+
+def getParcelOrder(id):
+    parcels = []
+    
+    for parcel in parcels:
+        parcels.append(parcel.to_json())
+    print(json_parcelOrders)
+    print(len(json_parcelOrders))  
+
+
+
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)        
+
